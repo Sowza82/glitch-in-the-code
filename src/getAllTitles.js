26ae5c7aa0eaef98fc4d7363780d6b30aee45b8b
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-17 22:39:59 -0300

    Entrega desafio 01

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,7 +1,9 @@
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
+if (!Array.isArray(movies)) return [];
+  return movies.map(movie => movie.title || movie.titulo);
+
 }
 
 module.exports = {
