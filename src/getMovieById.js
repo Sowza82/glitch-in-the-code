6af99256611532dc51732056e6b6b2bef73d25cc
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-18 00:07:33 -0300

    Entrega desafio 02

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
-  
+ if (typeof id !== "number") return undefined;
+  if (id <= 0) return undefined;
+
+  return movies.find(movie => movie.id === id);
+
 }
 
 module.exports = {
