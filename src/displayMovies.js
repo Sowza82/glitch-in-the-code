d508ad537b95600da5e44414d90e295ec63086f0
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-22 01:05:35 -0300

    Desafio 05

--- src/displayMovies.js
+++ src/displayMovies.js
@@ -1,7 +1,16 @@
 const { movies } = require("../data/list1");
 
 const displayMovies = (movies) => {
- 
+  if (!Array.isArray(movies)) {
+    throw new TypeError("O parâmetro 'movies' deve ser um array");
+  }
+
+  movies.forEach((movie) => {
+    if (movie.titulo && movie.diretor) {
+      console.log(`Título: ${movie.titulo} - Diretor: ${movie.diretor}`);
+    }
+  });
+
 }
 
 module.exports = {
