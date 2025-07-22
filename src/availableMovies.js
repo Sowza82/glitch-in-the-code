26bf0a63024a2406e6e7dcafd128674f7739020f
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-22 01:25:39 -0300

    Desafio 07

--- src/availableMovies.js
+++ src/availableMovies.js
@@ -1,7 +1,10 @@
 const movies = require("../data/movies");
 
 const availableMovies = (movies) => {
- 
+  return movies
+    .filter(movie => movie.disponivel === true)
+    .map(movie => movie.titulo);
+
 }
 
 module.exports = {
