7ad2ba7adfbc2a3babbc808795903c90aedd8109
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-22 00:40:31 -0300

    Desafio 03

--- src/getMoviesByActor.js
+++ src/getMoviesByActor.js
@@ -1,7 +1,21 @@
-const movies = require("../data/movies");
+const movies = require ("../data/movies");
 
-function getMoviesByActor(ator) {
- 
+function getMoviesByActor(actor) {
+
+ if (typeof actor !== "string" || actor.trim() === "") {
+    return [];
+  }
+
+  const nomeNormalizado = actor.trim().toLowerCase();
+
+  return movies.filter((movie) =>
+    movie.elenco.some(
+      (name) => name.trim().toLowerCase() === nomeNormalizado
+    )
+  );
+
+
+
 }
 
 module.exports = {
