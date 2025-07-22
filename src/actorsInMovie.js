d2f85c810571529a9bef3a4e8064d94fa7df1af0
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-22 01:51:38 -0300

    Desafio 10

--- src/actorsInMovie.js
+++ src/actorsInMovie.js
@@ -1,7 +1,14 @@
 const movies = require("../data/movies");
 
 const actorsInMovie = (titulo) => {
-  
+if (!titulo) return "Filme não encontrado.";
+
+  const movie = movies.find((m) => m.titulo === titulo);
+
+  if (!movie || !Array.isArray(movie.elenco)) return "Filme não encontrado.";
+
+  return movie.elenco.slice(0, 2);
+
 }
 
 module.exports = {
