e388430d5d5ce3431fac3f6509289191f7dd8950
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-22 01:15:27 -0300

    Desafio 06

--- src/orderMoviesByYear.js
+++ src/orderMoviesByYear.js
@@ -1,7 +1,15 @@
 const movies = require("../data/movies");
 
 const orderMoviesByYear = (movies) => {
-  
+  if (!Array.isArray(movies) || movies.length === 0) return [];
+
+
+  const moviesCopy = [...movies];
+
+ moviesCopy.sort((a, b) => a.anoLancamento - b.anoLancamento);
+
+  return moviesCopy;
+
 }
 
 module.exports = {
