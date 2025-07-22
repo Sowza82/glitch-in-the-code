cd9fbe4c1872cab6ff2b460cfb7e60f3a9923603
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-22 00:55:13 -0300

    Desafio 04

--- src/highestRatedMovie.js
+++ src/highestRatedMovie.js
@@ -1,7 +1,18 @@
 const movies = require("../data/movies");
 
 const highestRatedMovie = (movies) => {
-  
+if (!movies || movies.length === 0) return undefined;
+
+  let highestRated = movies[0];
+
+  for (let i = 1; i < movies.length; i++) {
+    if (movies[i].avaliacao > highestRated.avaliacao) {
+      highestRated = movies[i];
+    }
+  }
+
+  return highestRated;
+
 }
 
 module.exports = {
