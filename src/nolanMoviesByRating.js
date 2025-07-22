1741f78c38b55d12d13ed18f0015d61b7597de5f
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-22 01:31:15 -0300

    Desafio 08

--- src/nolanMoviesByRating.js
+++ src/nolanMoviesByRating.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 const nolanMoviesByRating = (movies) => {
- 
+  return movies
+    .filter(movie => movie.diretor === 'Christopher Nolan')
+    .sort((a, b) => b.avaliacao - a.avaliacao);
+
+
 }
 
 module.exports = {
