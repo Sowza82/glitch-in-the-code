1c9749eb4019151dfd04e3f26d1e397b5e28087d
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-23 22:22:04 -0300

    Desafio 11

--- src/averageRatingByGenre.js
+++ src/averageRatingByGenre.js
@@ -1,8 +1,32 @@
 const movies = require("../data/movies");
 function averageRatingByGenre(genero) {
+ if (genero === undefined || genero === '') {
+    return "O parâmetro é obrigatório.";
+  }
+
+  if (typeof genero !== "string") {
+    return "O parâmetro tem que ser uma string.";
+  }
+
+  const filmesDoGenero = movies.filter((filme) =>
+    filme.genero.includes(genero)
+  );
+
+  if (filmesDoGenero.length === 0) {
+    return `Não há filmes do gênero ${genero}.`;
+  }
+
+  const somaAvaliacoes = filmesDoGenero.reduce((total, filme) => {
+    return total + filme.avaliacao;
+  }, 0);
+
+  const media = somaAvaliacoes / filmesDoGenero.length;
+  const mediaFormatada = Number(media.toFixed(2));
+
+  return `A média de avaliação dos filmes do gênero ${genero} é ${mediaFormatada}.`;
 
 }
 
-module.exports = { 
+module.exports = {
   averageRatingByGenre
 }
