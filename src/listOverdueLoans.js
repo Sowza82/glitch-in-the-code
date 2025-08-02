5883b36fc1198398f31df6c9333355b16d891a26
Tatiane Souza <tathyannesg18@gmail.com>
2025-08-01 23:26:34 -0300

    Desafio 13 incompleto

--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,7 +1,22 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
- 
+ const today = new Date("2024-01-01");
+
+  return movies.flatMap(movie => {
+    if (!movie.emprestimos) return [];
+
+    return movie.emprestimos
+      .filter(({ dataDevolucao }) => new Date(dataDevolucao) < today)
+      .map(({ nomePessoa, dataEmprestimo, dataDevolucao }) => ({
+        id: movie.id,
+        titulo: movie.titulo,
+        nomePessoa,
+        dataEmprestimo,
+        dataDevolucao
+      }));
+  });
+
 }
 
 module.exports = {
