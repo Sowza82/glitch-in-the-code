912415363ec64ec407b0ba7c0dc6a73e590cd3b5
Tatiane Souza <tathyannesg18@gmail.com>
2025-08-01 23:44:33 -0300

    Desafio 10

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,7 +1,17 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+   const genreCount = {};
+
+  for (const movie of movies) {
+    if (Array.isArray(movie.genero) && movie.genero.length > 0) {
+      for (const genre of movie.genero) {
+        genreCount[genre] = (genreCount[genre] || 0) + 1;
+      }
+    }
+  }
+
+  return genreCount;
 }
 
 module.exports = {
